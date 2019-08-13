45d09da22a4ed66656bc95d510a4129f51cf95f2
Alan Ranciato <alanranciato@gmail.com>
2019-08-13 14:47:18 -0400

    removed healthcheck

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 	"net/http"
 )
 
-const version string = "1.0"
+const version string = "1.01"
 
 func getFrontpage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Congratulations! Version %s of your application is running on GKE.", version)
